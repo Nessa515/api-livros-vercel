5a15cdda97b174302789c26e9d34781eb6cb1f70
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2024-11-20 22:16:56 -0400

    Alterando arquivo vercel

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(route)
 app.use(cors());
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 
 export default app;
